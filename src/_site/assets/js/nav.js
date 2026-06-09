fd001de2d0ff0c0b0102cf333586c2a9b3e62861
Joe Baker <joesbaker@gmail.com>
2026-06-09 15:03:34 +0100

    Accidentally committed

--- src/_site/assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const currentPath = window.location.pathname;
-  document.querySelectorAll(".topnav a").forEach((link) => {
-    if (link.getAttribute("href") === currentPath) {
-      link.setAttribute("aria-current", "page");
-    }
-  });
-});
