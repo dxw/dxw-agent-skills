45692cb208857e1c8d49349485103c30e4bffb50
Joe Baker <joesbaker@gmail.com>
2026-06-18 17:40:00 +0100

    Markup partials, styling for lists

--- site/src/_data/navigation.js
+++ site/src/_data/navigation.js
@@ -1,6 +1,7 @@
 export default () => [
   { name: "Getting Started", url: "/getting-started/" },
   { name: "Skills", url: "/skills/" },
+  { name: "Guides", url: "/guides/" },
   { name: "Versions", url: "/versions/" },
   { name: "Changelog", url: "/changelog/" },
 ];
